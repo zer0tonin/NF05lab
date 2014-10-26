35f96c9d391e2ec2fdb976c9bbba150e3691edde
Antoine Girard Guittard <bouhahah@gmail.com>
2014-10-26 19:52:20 +0100

    Rajout de ; à calculs.h

--- Calculs.h
+++ Calculs.h
@@ -3,13 +3,13 @@
 
 #include "Matrice.h"
 
-Matrice operator+(const Matrice &M1, const Matrice &M2)
-Matrice operator-(const Matrice &M1, const Matrice &M2)
-Matrice operator*(const Matrice &M1, const Matrice &M2)
-Matrice operator/(const Matrice &M1, const Matrice &M2)
-Matrice operator*(const Matrice &M1, float Scalaire)
-Matrice operator*(float Scalaire, const Matrice &M1)
-Matrice operator/(const Matrice &M1, float Scalaire)
-float ProduitScalaire(Matrice M1, Matrice M2)
+Matrice operator+(const Matrice &M1, const Matrice &M2);
+Matrice operator-(const Matrice &M1, const Matrice &M2);
+Matrice operator*(const Matrice &M1, const Matrice &M2);
+Matrice operator/(const Matrice &M1, const Matrice &M2);
+Matrice operator*(const Matrice &M1, float Scalaire);
+Matrice operator*(float Scalaire, const Matrice &M1);
+Matrice operator/(const Matrice &M1, float Scalaire);
+float ProduitScalaire(Matrice M1, Matrice M2);
 
 #endif
