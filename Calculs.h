b7cbccd6c0f5c7d4bfe45db9f2458fb209aa0c39
Antoine Girard Guittard <bouhahah@gmail.com>
2014-10-26 19:11:14 +0100

    Mise à jour de calculs.h

--- Calculs.h
+++ Calculs.h
@@ -3,12 +3,13 @@
 
 #include "Matrice.h"
 
-Matrice SommeMatrice(Matrice M1, Matrice M2);
-Matrice MultiplicationMatrice(Matrice M1, Matrice M2);
-Matrice DivisionMatrice(Matrice M1, Matrice M2);
-float ProduitScalaire(Matrice M1, Matrice M2);
-
-Matrice MultiplicationMatriceScalaire(Matrice M1, float Scalaire);
-Matrice DivisionMatriceScalaire(Matrice M1, float Scalaire);
+Matrice operator+(const Matrice &M1, const Matrice &M2)
+Matrice operator-(const Matrice &M1, const Matrice &M2)
+Matrice operator*(const Matrice &M1, const Matrice &M2)
+Matrice operator/(const Matrice &M1, const Matrice &M2)
+Matrice operator*(const Matrice &M1, float Scalaire)
+Matrice operator*(float Scalaire, const Matrice &M1)
+Matrice operator/(const Matrice &M1, float Scalaire)
+float ProduitScalaire(Matrice M1, Matrice M2)
 
 #endif
