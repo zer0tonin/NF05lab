a906fc6249c055f0b4b9c0e9c6fed4b7c4fa1b47
victorlevasseur <victorlevasseur52@gmail.com>
2015-01-04 00:02:26 +0100

    Quelques fix pour les fenêtre ResolutionSystemeDialogue

--- ResolutionSystemeDialogue.cpp
+++ ResolutionSystemeDialogue.cpp
@@ -19,6 +19,18 @@ void ResolutionSystemeDialogue::SurClicBoutonValiderSysteme(wxCommandEvent& even
 	
 	if(nombreEquations > 0)
 	{
+		//On supprime les colonnes et les lignes des deux grilles
+		if(m_tableauMatriceSysteme->GetRows() > 0)
+			m_tableauMatriceSysteme->DeleteRows(0, m_tableauMatriceSysteme->GetRows());
+		if(m_tableauMatriceSysteme->GetCols() > 0)
+			m_tableauMatriceSysteme->DeleteCols(0, m_tableauMatriceSysteme->GetCols());
+		
+		if(m_tableauVecteurSysteme->GetRows() > 0)
+			m_tableauVecteurSysteme->DeleteRows(0, m_tableauVecteurSysteme->GetRows());
+		if(m_tableauVecteurSysteme->GetCols() > 0)
+			m_tableauVecteurSysteme->DeleteCols(0, 1);
+		
+		//On ajoute le nombre de colonnes et de lignes nécessaires
 		m_tableauMatriceSysteme->AppendRows(nombreEquations);
 		m_tableauMatriceSysteme->AppendCols(nombreEquations);
 		
